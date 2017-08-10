9d796d49f81f63575b7eca58365a534572c416af
JackLingwood <jack.lingwood@gmail.com>
2017-08-10 02:32:20 -0400

    Initial check-in

---
+++ CSharpGenerations/CSharp7/InlineOutVariables.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp7
+{
+
+    public class InlineOutVariables
+    {
+        public void Before()
+        {
+            int numericResult;
+            int.TryParse("45", out numericResult);
+        }
+
+        public void Now1()
+        {
+            int.TryParse("45", out int numericResult);
+        }
+
+        public void Now2()
+        {
+            int.TryParse("45", out var numericResult);
+        }
+    }
+}
